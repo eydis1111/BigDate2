efc6c17b21713a96142970921f96950f86926345
Daelim Ji <202130130@daelim.ac.kr>
2024-10-31 12:14:32 +0900

    v2.5

--- week10/wee10.py
+++ week10/wee10.py
@@ -1,13 +1,9 @@
 import urllib.request
-
-
 import pandas as pd
 from bs4 import BeautifulSoup
 import datetime
 
-
-
-
+#print(datetime.datetime.now().strftime("%Y년 %m월 %d일 %H시 %M분 %S초"))
 
 shops = []
 
@@ -25,8 +21,8 @@ for i in range(1, 51):
         shop_addr = tds[3].string  # 주소
         shop_phone = tds[5].string  # 전화번호
 
-        shops.append([shop_name]+[shop_addr]+[shop_phone]+[datetime.datetime.now().strftime("%Y년 %m눨 %d일 %H시 %M분 %S초")])  # 2d list
+        shops.append([shop_name]+[shop_addr]+[shop_phone]+[datetime.datetime.now().strftime("%Y년 %m월 %d일 %H시 %M분 %S초")])  # 2d list
 
 #print(shops)
 hollys_df = pd.DataFrame(shops, columns=('매장이름', '주소', '전화번호', '일시'))
-hollys_df.to_csv('hollys.csv', encoding= 'cp949', mode='w')
+hollys_df.to_csv('hollys.csv', encoding='cp949', mode='w')
