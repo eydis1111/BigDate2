e4758e24ad583b557b01c85b09d396ef9d26beb3
Daelim Ji <202130130@daelim.ac.kr>
2024-11-07 10:52:17 +0900

    v2.8

--- week11/week11.py
+++ week11/week11.py
@@ -2,16 +2,29 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#성별에 따른 생존률 계산
+titanic = sns.load_dataset('titanic')
+# print(titanic['sex'].head())
+# print(titanic.info())
+gender_survival = titanic.groupby(by='sex')['survived'].mean()
+print(gender_survival)
+
+
+
+
+
+
+
 
 #print(titanic['survived'])
 #print(titanic.info())
 
 #생존자 수와 사망자 수 시각화
-titanic = sns.load_dataset('titanic')
-sns.countplot(data=titanic, x='survived')
-plt.title('Survived (0 = NO, 1 = Yes)')
-plt.xlabel('Survived')
-plt.ylabel('Count')
-plt.show()
+
+# sns.countplot(data=titanic, x='survived')
+# plt.title('Survived (0 = NO, 1 = Yes)')
+# plt.xlabel('Survived')
+# plt.ylabel('Count')
+# plt.show()
 
 
